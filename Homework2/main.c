4c7d181051c5ec493fed23591ffc9124d791b512
David Dagson <dagson@student.chalmers.se>
2016-12-05 13:59:55 +0100

    Initial Commit

---
+++ Homework2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Homework2
+//
+//  Created by David Dagson on 2016-12-05.
+//  Copyright © 2016 David Dagson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
